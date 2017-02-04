97480c63e4709600fa1ec5d8af7fe6005c26c8c4
Will Stevens <williamstevens@gmail.com>
2017-02-04 00:04:53 -0500

    updated leaderboard and added a script to deploy all teams

---
+++ _deploy_all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./_build.sh iris && ./_deploy.sh iris
+./_build.sh royal && ./_deploy.sh royal
+./_build.sh 6ixers && ./_deploy.sh 6ixers
+./_build.sh stella && ./_deploy.sh stella
