eff8f4c0a79c5136e88b6215bdfe04dd93054cb0
Will Stevens <williamstevens@gmail.com>
2019-03-25 09:18:17 -0400

    added team canada (4 teams), traffic and uwl

--- _run.sh
+++ _run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# build the theme components (first arg = theme name)
-source __teams.sh
-
-# build and run the application
-esc -o static.go static && go build && ./ultitracker
+./bin/ultitracker_darwin_amd64
